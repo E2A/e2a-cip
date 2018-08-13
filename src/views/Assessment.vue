e3e10c6a3eaa0ee607a60683c225faf0151f4290
Scott Schaffter <scott@bivee.co>
2018-08-13 12:09:13 -0600

    linting

--- src/views/Assessment.vue
+++ src/views/Assessment.vue
@@ -68,7 +68,7 @@ export default {
   data () {
     return {
       groupedActivities: this.getGroupedActivites(),
-      cipTitle: this.getItemValue('setup','title')
+      cipTitle: this.getItemValue('setup', 'title')
     }
   }
 }
