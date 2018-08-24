21b9706fe6f035945f756f6c04ba5318371ab1e2
Scott Schaffter <scott@bivee.co>
2018-08-24 13:16:01 -0600

    setup new activity count data

--- src/components/mixins/helpers.js
+++ src/components/mixins/helpers.js
@@ -5,6 +5,11 @@ export const stringHelpers = {
     // https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
     capitalize: function (string) {
       return `${string.charAt(0).toUpperCase()}${string.slice(1)}`
+    },
+    titleCase: function (str) {
+      return str.toLowerCase().split(' ').map((word) => {
+        return (word.charAt(0).toUpperCase() + word.slice(1))
+      }).join(' ')
     }
   }
 }
