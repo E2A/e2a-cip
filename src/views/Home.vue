275b6a6cd6cea63d36551afcc2ab971536c7d90b
eliseoeric <eric.eliseo@gmail.com>
2019-09-08 14:32:19 -0500

    Use slots for BasePageIntro
    In order to accommodate various layouts in the page intro, this commit
    refactors the `BasePageIntro` component to use a slot. This allows us to
    have a different layout on the `Home` view, but keep keep the standard
    layout in future pages.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,9 @@
 <template>
   <article>
-    
     <aside
       :class="[space.paddingHorizontal, space.paddingVerticalNarrow, color.well, border.bottom, type.right]"
     >
-      <BaseGutterWrapper
-        gutterY="xnarrow"
-        gutterX="xnarrow"
-      >
+      <BaseGutterWrapper gutterY="xnarrow" gutterX="xnarrow">
         <BaseHeading
           :class="display.inlineBlock"
           :centered="false"
@@ -15,9 +11,7 @@
           color="dark"
           weight="regular"
           sub
-        >
-          {{$t('home.chooseLanguage')}}
-        </BaseHeading>
+        >{{$t('home.chooseLanguage')}}</BaseHeading>
         <!-- Language selector -->
         <LanguageSwitcher />
       </BaseGutterWrapper>
@@ -26,33 +20,50 @@
     <BaseWidthWrapper :class="[space.paddingVerticalWide, space.paddingHorizontal, border.bottom]">
       <BaseGutterWrapper :class="base.logoGrid">
         <div :class="base.logo">
-          <img
-            src="@/assets/images/logos/usaid-lockup.svg"
-            alt="USAID and E2A"
-          />
+          <img src="@/assets/images/logos/usaid-lockup.svg" alt="USAID and E2A" />
         </div>
         <div :class="base.logoSmall">
-          <img
-            src="@/assets/images/logos/pathfinder.png"
-            alt="Pathfinder"
-          />
+          <img src="@/assets/images/logos/pathfinder.png" alt="Pathfinder" />
         </div>
         <div :class="base.logoSmall">
-          <img
-            src="@/assets/images/logos/ouaga.png"
-            alt="OP"
-          />
+          <img src="@/assets/images/logos/ouaga.png" alt="OP" />
         </div>
       </BaseGutterWrapper>
     </BaseWidthWrapper>
 
     <!-- Intro -->
-    <BasePageIntro
-      :title="$t('home.title')"
-      :subtitle="$t('home.subtitle')"
-      :introHeader="$t('home.introHeader')"
-      :blurb="$t('home.intro')"
-    />
+    <BasePageIntro>
+      <BaseWidthWrapper :class="[base.wrapper, base.headlineWrapper]" width="wide">
+        <BaseGutterWrapper :class="base.col12">
+          <BaseHeading
+            :class="[space.paddingBottom, base.title]"
+          >{{ $t('home.title') }}</BaseHeading>
+        </BaseGutterWrapper>
+      </BaseWidthWrapper>
+
+      <BaseWidthWrapper :class="[base.wrapper]" width="wide">
+        <div :class="base.col6">
+          <BaseHeading
+            :class="space.paddingBottom"
+            scale="delta"
+            sub
+            :centered="false"
+          >{{ $t('home.subtitle') }}</BaseHeading>
+          <BaseHeading
+            :class="space.paddingBottom"
+            scale="delta"
+            sub
+            :centered="false"
+          >{{ $t('home.introHeader') }}</BaseHeading>
+        </div>
+
+        <div :class="[base.col6, color.well]">
+          <BaseWidthWrapper>
+            <BaseBodyText :content="$t('home.intro')" />
+          </BaseWidthWrapper>
+        </div>
+      </BaseWidthWrapper>
+    </BasePageIntro>
 
     <!-- How it works -->
     <BaseSectionWrapper>
@@ -60,14 +71,8 @@
         <BaseHeading :level="2" centered>{{$t('home.stepsTitle')}}</BaseHeading>
         <BaseHeading centered :level="5" color="dark">{{$t('home.stepsIntro')}}</BaseHeading>
       </BaseWidthWrapper>
-      <BaseWidthWrapper
-        width="xwide"
-        :class="space.paddingTop"
-      >
-        <BaseStepList
-          :steps="steps"
-          size="small"
-        />
+      <BaseWidthWrapper width="xwide" :class="space.paddingTop">
+        <BaseStepList :steps="steps" size="small" />
       </BaseWidthWrapper>
     </BaseSectionWrapper>
 
@@ -80,33 +85,15 @@
         role="primary"
       />
       <div>
-        <ClearItems
-          :clearType="['All']"
-          :class="space.marginTop"
-        />
+        <ClearItems :clearType="['All']" :class="space.marginTop" />
       </div>
     </BaseSectionWrapper>
 
     <!-- Offline versions -->
-    <BaseSectionWrapper
-      v-if="!electron"
-      :class="color.well"
-      el="div"
-      border
-    >
-      <BaseHeading
-        :level="2"
-        centered
-      >
-        {{$t('home.downloadTitle')}}
-      </BaseHeading>
-      <BaseWidthWrapper
-        :class="space.paddingTop"
-      >
-        <BaseBodyText
-          :content="$t('home.downloadBlurb')"
-          :class="base.centeredSubheads"
-        />
+    <BaseSectionWrapper v-if="!electron" :class="color.well" el="div" border>
+      <BaseHeading :level="2" centered>{{$t('home.downloadTitle')}}</BaseHeading>
+      <BaseWidthWrapper :class="space.paddingTop">
+        <BaseBodyText :content="$t('home.downloadBlurb')" :class="base.centeredSubheads" />
         <BaseGutterWrapper
           :class="[space.paddingTop, type.center]"
           gutterX="xnarrow"
@@ -132,15 +119,8 @@
 
     <!-- Acknowledgements -->
     <BaseSectionWrapper border>
-      <BaseHeading
-        :level="2"
-        scale="gamma"
-      >
-        {{$t('home.acknowldgementTitle')}}
-      </BaseHeading>
-      <BaseWidthWrapper
-        :class="space.paddingTop"
-      >
+      <BaseHeading :level="2" scale="gamma">{{$t('home.acknowldgementTitle')}}</BaseHeading>
+      <BaseWidthWrapper :class="space.paddingTop">
         <BaseBodyText
           :content="$t('home.acknowledgementText')"
           :class="base.centeredSubheads"
@@ -152,22 +132,22 @@
 </template>
 
 <script>
-import i18n from '@/i18n.js'
-import LanguageSwitcher from '@/components/LanguageSwitcher.vue'
-import BasePageIntro from '@/components/BasePageIntro.vue'
-import BaseWidthWrapper from '@/components/BaseWidthWrapper.vue'
-import BaseSectionWrapper from '@/components/BaseSectionWrapper.vue'
-import BaseHeading from '@/components/BaseHeading.vue'
-import BaseBodyText from '@/components/BaseBodyText.vue'
-import BaseGutterWrapper from '@/components/BaseGutterWrapper.vue'
-import BaseStepList from '@/components/BaseStepList.vue'
-import BaseButtonLink from '@/components/BaseButtonLink.vue'
-import ClearItems from '@/components/ClearItems.vue'
-import { dataMethods } from '@/components/mixins/dataMethods'
-import { initData } from '@/components/mixins/initData.js'
+import i18n from "@/i18n.js";
+import LanguageSwitcher from "@/components/LanguageSwitcher.vue";
+import BasePageIntro from "@/components/BasePageIntro.vue";
+import BaseWidthWrapper from "@/components/BaseWidthWrapper.vue";
+import BaseSectionWrapper from "@/components/BaseSectionWrapper.vue";
+import BaseHeading from "@/components/BaseHeading.vue";
+import BaseBodyText from "@/components/BaseBodyText.vue";
+import BaseGutterWrapper from "@/components/BaseGutterWrapper.vue";
+import BaseStepList from "@/components/BaseStepList.vue";
+import BaseButtonLink from "@/components/BaseButtonLink.vue";
+import ClearItems from "@/components/ClearItems.vue";
+import { dataMethods } from "@/components/mixins/dataMethods";
+import { initData } from "@/components/mixins/initData.js";
 
 export default {
-  name: 'Home',
+  name: "Home",
   mixins: [dataMethods, initData],
   components: {
     LanguageSwitcher,
@@ -182,21 +162,23 @@ export default {
     ClearItems
   },
   computed: {
-    steps () {
-      return Object.values(i18n.messages[i18n.locale].home.steps).map((step, index) => {
-        return {
-          title: this.$t(`home.steps.step${index + 1}.title`),
-          blurb: this.$t(`home.steps.step${index + 1}.blurb`)
+    steps() {
+      return Object.values(i18n.messages[i18n.locale].home.steps).map(
+        (step, index) => {
+          return {
+            title: this.$t(`home.steps.step${index + 1}.title`),
+            blurb: this.$t(`home.steps.step${index + 1}.blurb`)
+          };
         }
-      })
+      );
     }
   },
-  data () {
+  data() {
     return {
       electron: this.checkElectron()
-    }
+    };
   }
-}
+};
 </script>
 
 <style src="styles/spacing.scss" lang="scss" module="space"></style>
@@ -206,9 +188,10 @@ export default {
 <style src="styles/display.scss" lang="scss" module="display"></style>
 
 <style lang="scss" module="base">
-@import '~styleConfig/breakpoints';
-@import '~styleConfig/scale';
-@import '~styleConfig/type';
+@import "~styleConfig/breakpoints";
+@import "~styleConfig/scale";
+@import "~styleConfig/type";
+@import "~styleConfig/spacing";
 
 .logoGrid {
   text-align: center;
@@ -219,7 +202,7 @@ export default {
   max-width: 36rem;
   vertical-align: middle;
 
-  @include media('>small') {
+  @include media(">small") {
     min-width: 30rem;
   }
 
@@ -234,7 +217,7 @@ export default {
   composes: logo;
   max-width: 16em;
 
-  @include media('>small') {
+  @include media(">small") {
     min-width: 13rem;
   }
 }
@@ -243,7 +226,7 @@ export default {
   // center all heading elements
   $list: ();
   @each $i in [1, 2, 3, 4, 5, 6] {
-    $list: append($list, unquote('h#{$i}'), 'comma');
+    $list: append($list, unquote("h#{$i}"), "comma");
   }
 
   #{$list} {
@@ -253,5 +236,24 @@ export default {
 
 .gutterItem {
   display: inline-block;
+}
+
+.wrapper {
+  display: flex;
+  flex-direction: row;
+  padding: space("medium") space("narrow");
+}
+
+.col6 {
+  flex: 1;
+  max-width: 50%;
+  flex-basis: 50%;
+  padding: space("medium") space("medium");
+}
+
+.col12 {
+  flex: 1;
+  max-width: 100%;
+  flex-basis: 100%;
 }
 </style>
